cad66dd7272dc456cfa3e85c6cbed97bd63afd47
Juan Salvador Barbera <86657@sistemas.frc.utn.edu.ar>
2024-09-18 19:31:45 -0300

    Bugfix-Se arregla el envío duplicado de mails

--- TrabajosPracticos/TrabajoPractico6/EstructuraCodigo/Backend/src/services/emailService.js
+++ TrabajosPracticos/TrabajoPractico6/EstructuraCodigo/Backend/src/services/emailService.js
@@ -20,7 +20,7 @@ module.exports = {
             let transportistasRetiro = await getTransportistasByLocalidad(pedido.domicilioRetiro.id_localidad);
             let transportistasEntrega = await getTransportistasByLocalidad(pedido.domicilioRetiro.id_localidad);
             let transportistas = transportistasRetiro.concat(transportistasEntrega);
-            let transportistasSinDuplicar = transportistas.filter((transportista, index) => transportistas.indexOf(transportista) === index); // Eliminar duplicados
+            let transportistasSinDuplicar = transportistas.filter((transportista, index, self) => index === self.findIndex(t => t.id_transportista === transportista.id_transportista)); // Eliminar duplicados
             let tipoCarga = await soporteOrm.getAll("TiposCarga");
             tipoCarga = tipoCarga.filter((tipo) => tipo.id === pedido.tipoCarga)[0].nombre;        
             let calleRetiro = pedido.domicilioRetiro.calle;
@@ -46,7 +46,8 @@ module.exports = {
             Domicilio de Entrega: ${calleEntrega} ${numeroEntrega}, ${localidadEntrega}, ${provinciaEntrega}, ${referenciaEntrega && 'Referencia: ' + referenciaEntrega} \n
             Fecha de Retiro: ${fechaRetiro} \n
             Fecha de Entrega: ${fechaEntrega} \n
-            TangoApp®`;     
+            TangoApp®`; 
+            
             
             // Procesar las imágenes en base64
             let attachments = pedido.imagenes.map((imagenBase64, index) => {
